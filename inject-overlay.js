8dc0daa21cd56f908d432c52918614686ae09fd5
Blaise Kal <blaisekal@gmail.com>
2017-10-24 12:33:34 +0200

    Administrate todos

--- inject-overlay.js
+++ inject-overlay.js
@@ -1,9 +1,9 @@
+// TODO: Send message to background.js when the overlay is closed.
 // Create a new scope each time the extension is activated.
-(function() {
+(function () {
 
     // Setup full-window frame.
     const frame = document.createElement('iframe');
-    frame.id = 'extmc-overlay'
     frame.style.position = 'fixed';
     frame.style.width = '100vw';
     frame.style.height = '100vh';
@@ -12,14 +12,13 @@
     frame.style.border = 'none';
     frame.style.zIndex = Math.pow(2, 24).toString();
     frame.src = browser.extension.getURL("media-carousel.html");
-    document.documentElement.appendChild(frame);
 
     // TODO: Prevent scrolling the parent frame while carousel is visible.
     // Send the current document to the frame to extract media.
-    frame.addEventListener('load', () => {
-        let element = document.body ? document.body : document.documentElement;
+    frame.addEventListener('load', (event) => {
+        const element = document.body ? document.body : document.documentElement;
         frame.contentWindow.postMessage(element.innerHTML, '*');
-        frame.contentWindow.focus();
+        frame.contentWindow.focus(); // TODO: Keys aren't captured by frame; focus differently
     });
 
     window.addEventListener('message', (event) => {
@@ -27,5 +26,7 @@
             document.documentElement.removeChild(frame);
         }
     });
+
+    document.documentElement.appendChild(frame);
 
 })();
